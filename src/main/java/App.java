006b8621771daa76731682b5789ae7f0563decff
frank7841 <franklinewax@gmail.com>
2020-07-20 19:44:16 +0300

    Created  to APi route to post new department

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,8 +1,11 @@
 import com.google.gson.Gson;
 import dao.*;
+import models.Department;
 import org.sql2o.Sql2o;
 
 import java.sql.Connection;
+
+import static spark.Spark.post;
 
 public class App {
     public static void main(String[] args){
@@ -17,5 +20,13 @@ public class App {
         departmentNewsDao=new Sql2oDepartmentNewsDao(sql2o);
         generalNewsDao=new Sql2oGeneralNewsDao(sql2o);
         userDao=new Sql2oUsersDao(sql2o);
+
+
+        post("/departments/new", "application/json", (req, res) -> {
+            Department department = gson.fromJson(req.body(), Department.class);
+            departmentDao.add(department);
+            res.status(201);
+            return gson.toJson(department);
+        });
     }
 }
