98e3557b45f145ff2c875abe31b81af68e677ae7
frank7841 <franklinewax@gmail.com>
2020-07-20 19:30:21 +0300

    Importing necessary packages

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+import com.google.gson.Gson;
+import dao.*;
+import org.sql2o.Sql2o;
+
+import java.sql.Connection;
 
 public class App {
+    public static void main(String[] args){
+        Sql2oDepartmentDao departmentDao;
+        Sql2oDepartmentNewsDao departmentNewsDao;
+        Sql2oGeneralNewsDao generalNewsDao;
+        Sql2oUsersDao userDao;
+        Connection conn;
+        Gson gson=new Gson();
+        Sql2o sql2o = DB.sql2o;
+        departmentDao=new Sql2oDepartmentDao(sql2o);
+        departmentNewsDao=new Sql2oDepartmentNewsDao(sql2o);
+        generalNewsDao=new Sql2oGeneralNewsDao(sql2o);
+        userDao=new Sql2oUsersDao(sql2o);
+    }
 }
