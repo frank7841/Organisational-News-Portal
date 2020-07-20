632c02b277c308fa50d1847edef93250b9ffb661
frank7841 <franklinewax@gmail.com>
2020-07-20 11:36:36 +0300

    Created Test methods to get and set content

--- src/test/java/models/GeneralNewsTest.java
+++ src/test/java/models/GeneralNewsTest.java
@@ -1,7 +1,43 @@
 package models;
 
 import junit.framework.TestCase;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
-public class GeneralNewsTest{
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+public class GeneralNewsTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    public GeneralNews setUpGeneralNews() {
+        return new GeneralNews("Agm meeting date", 2);
+    }
+
+    @Test
+    public void GeneralNews_instanciatesCorrectly_true() {
+        GeneralNews testGeneralNews = setUpGeneralNews();
+        assertEquals(true, testGeneralNews instanceof News);
+    }
+
+    @Test
+    public void getContent_returnsCorrectCOntent() {
+        GeneralNews testGeneralNews = setUpGeneralNews();
+        assertEquals("Agm meeting date", testGeneralNews.getContent());
+    }
+
+    @Test
+    public void setContent_true() {
+        GeneralNews testGeneralNews = setUpGeneralNews();
+        testGeneralNews.setContent("Company layoffs");
+        assertNotEquals("Agm meeting date ", testGeneralNews.getContent());
+    }
 
 }
