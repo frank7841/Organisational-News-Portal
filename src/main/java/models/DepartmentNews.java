08e1343b3618334c7e8ccb6b131858663ed1521b
frank7841 <franklinewax@gmail.com>
2020-07-20 10:06:32 +0300

    Created getters and setter methods for class DepartmentNews

--- src/main/java/models/DepartmentNews.java
+++ src/main/java/models/DepartmentNews.java
@@ -2,13 +2,33 @@ package models;
 
 public class DepartmentNews extends News{
     private int id;
-    private int departmentid;
+    private int departmentId;
     private static final String DATABASE_TYPE="department";//constant
 
-    public DepartmentNews(String content, int userid, int departmentid) {
+    public DepartmentNews(String content, int userid, int departmentId) {
         this.content=content;
         this.userid=userid;
-        this.departmentid=departmentid;
+        this.departmentId=departmentId;
         type = DATABASE_TYPE;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getDepartmentid() {
+        return departmentId;
+    }
+
+    public void setDepartmentid(int departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public static String getDatabaseType() {
+        return DATABASE_TYPE;
     }
 }
