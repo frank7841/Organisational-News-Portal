45bd1e4b1b6e971c2f57d42c1999181be0662052
frank7841 <franklinewax@gmail.com>
2020-07-20 09:20:25 +0300

    Created Test to return correct Username

--- src/test/java/models/UsersTest.java
+++ src/test/java/models/UsersTest.java
@@ -8,7 +8,7 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 
-public class UsersTest extends TestCase {
+public class UsersTest{
     @Before
     public void setUp() throws Exception {
     }
@@ -17,12 +17,17 @@ public class UsersTest extends TestCase {
     public void tearDown() throws Exception {
     }
     public Users setUpUsers(){
-        return new Users("Frankline","CTO","Oversee ICT",1);
+        return new Users("frankline","CTO","Oversee ICT",1);
     }
     @Test
     public void instanciatesCorrectly_true(){
         Users testUser=setUpUsers();
         assertEquals(true,testUser instanceof Users);
+    }
+    @Test
+    public void getName_returnsCorrectName(){
+        Users testUser=setUpUsers();
+        assertEquals("frankline",testUser.getUsername());
     }
 
 }
