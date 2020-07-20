41a6286fbabb727e97c727ca2edaac06bda9b066
frank7841 <franklinewax@gmail.com>
2020-07-20 10:11:39 +0300

    DepartmentNewsClass Instantiated cotrrectly test

--- src/test/java/models/DepartmentNewsTest.java
+++ src/test/java/models/DepartmentNewsTest.java
@@ -1,7 +1,29 @@
 package models;
 
 import junit.framework.TestCase;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
-public class DepartmentNewsTest extends TestCase {
+import static org.junit.Assert.assertEquals;
+
+public class DepartmentNewsTest{
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+    public DepartmentNews setUpDepartmentNews(){
+        return new DepartmentNews("Check Your emails for meeting transcript",1,1);
+    }
+    @Test
+    public void DepartmentNews_instanciatesCorrectly_true(){
+        DepartmentNews testDepartmentNews=setUpDepartmentNews();
+        assertEquals(true,testDepartmentNews instanceof News);
+    }
+
 
 }
