df724bf9b5c8365d05985cf99c6a8b4e63d1fc5a
frank7841 <franklinewax@gmail.com>
2020-07-20 18:26:24 +0300

    Overriding getall method

---
+++ src/main/java/dao/Sql2oGeneralNewsDao.java
@@ -0,0 +1,38 @@
+package dao;
+
+import models.GeneralNews;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+import org.sql2o.Sql2oException;
+
+import java.util.List;
+
+public class Sql2oGeneralNewsDao implements GeneralNewsDao {
+    private final Sql2o sql2o;
+    public Sql2oGeneralNewsDao(Sql2o sql2o){
+        this.sql2o=sql2o;
+    }
+
+    @Override
+    public void add(GeneralNews generalNews){
+        String sql="INSERT INTO news (content , userid , type) VALUES (:content , :userid , :type)";
+        try(Connection con=sql2o.open()){
+            int id=(int) con.createQuery(sql,true)
+                    .bind(generalNews)
+                    .executeUpdate()
+                    .getKey();
+            generalNews.setId(id);
+        }catch (Sql2oException ex) {
+            System.out.println(ex);
+        }
+    }
+    @Override
+    public List<GeneralNews> getAll(){
+        try(Connection con= sql2o.open()){
+            return con.createQuery("SELECT * FROM news WHERE type='general' ")
+                    .throwOnMappingFailure(false)
+                    .executeAndFetch(GeneralNews.class);
+        }
+    }
+
+}
