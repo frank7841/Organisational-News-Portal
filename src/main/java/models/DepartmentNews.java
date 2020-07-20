2ca478aa00a69ef72097a9a915d405dd6120bc4c
frank7841 <franklinewax@gmail.com>
2020-07-20 10:03:45 +0300

    Declaring objects for class Department news

--- src/main/java/models/DepartmentNews.java
+++ src/main/java/models/DepartmentNews.java
@@ -5,4 +5,10 @@ public class DepartmentNews extends News{
     private int departmentid;
     private static final String DATABASE_TYPE="department";//constant
 
+    public DepartmentNews(String content, int userid, int departmentid) {
+        this.content=content;
+        this.userid=userid;
+        this.departmentid=departmentid;
+        type = DATABASE_TYPE;
+    }
 }
