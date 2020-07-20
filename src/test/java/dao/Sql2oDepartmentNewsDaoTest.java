e935b52f4f0d94722087938294114261bfb5f22d
frank7841 <franklinewax@gmail.com>
2020-07-20 17:57:07 +0300

    Created test method return department news with set Id

--- src/test/java/dao/Sql2oDepartmentNewsDaoTest.java
+++ src/test/java/dao/Sql2oDepartmentNewsDaoTest.java
@@ -1,7 +1,45 @@
 package dao;
 
 import junit.framework.TestCase;
+import models.DepartmentNews;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.sql2o.Connection;
+
+import static org.junit.Assert.assertNotEquals;
 
 public class Sql2oDepartmentNewsDaoTest {
+    public DepartmentNews setupNewDepartmentNews(){
+        return new DepartmentNews("We are having a gdepartment meeting",1,2);
+    }
+    public DepartmentNews setupNewDepartmentNews1(){ return new DepartmentNews("Projects should be submitted to the project supervisors ",2,3);
+    }
+    private static Sql2oDepartmentNewsDao departmentNewsDao;
+    private static Connection con;
+
+    @BeforeClass
+    public static void setUp() throws Exception {
+        departmentNewsDao=new Sql2oDepartmentNewsDao(DB.sql2o);
+        con = DB.sql2o.open();
+    }
+    @After
+    public void tearDown() throws Exception {
+        System.out.println("clearing database");
+        departmentNewsDao.deleteAll();
+    }
+    @AfterClass
+    public static void shutDown() throws Exception {
+        con.close(); // close connection once after this entire test file is finished
+        System.out.println("connection closed");
+    }
+    @Test
+    public void addingDepartmentNewsSetsId() throws Exception {
+        DepartmentNews testDepartmentNews = setupNewDepartmentNews();
+        int originalDepartmentNewsId = testDepartmentNews.getId();
+        departmentNewsDao.add(testDepartmentNews);
+        assertNotEquals(originalDepartmentNewsId,testDepartmentNews.getId());
+    }
 
 }
