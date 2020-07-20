3f048a32e00cf093b188cdf1dfb6335d74ef7c19
frank7841 <franklinewax@gmail.com>
2020-07-20 09:10:54 +0300

    Added created UserTest Class

---
+++ src/test/java/models/UsersTest.java
@@ -0,0 +1,18 @@
+package models;
+
+import junit.framework.TestCase;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class UsersTest extends TestCase {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+   }
